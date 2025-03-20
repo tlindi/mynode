692109a958510514b027c0685855a4da3e3c1166
Tomi Lind <tomi@lind.fi>
2025-03-20 23:12:24 +0200

    Add albyhub (v1.15.0)
    Fix albyhub.service: no syslog (btw mynode_sdk .service file needs updateting)
    Update albyhub.png - Use GetAlby Brand Kit logo

--- rootfs/standard/usr/share/mynode_apps/albyhub/scripts/install_albyhub.sh
+++ rootfs/standard/usr/share/mynode_apps/albyhub/scripts/install_albyhub.sh
@@ -28,5 +28,3 @@ docker images --format '{{.Repository}}:{{.Tag}}' | grep 'ghcr.io/getalby/hub' |
 # Pull ready dockers, instead of source
 docker pull ghcr.io/getalby/hub:$VERSION
 docker tag ghcr.io/getalby/hub:$VERSION albyhub
-
-echo "================== DONE INSTALLING APP ================="
